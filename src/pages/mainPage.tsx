d8521bcc96201bc20ff5659102c5de6c99ea199d
khaulanauman <khaulanauman@gmail.com>
2024-12-01 23:00:41 +0500

    added font

--- src/pages/mainPage.tsx
+++ src/pages/mainPage.tsx
@@ -1,9 +1,12 @@
+import BackgroundAudio from "../components/BackgroundAudio";
+
 export default function MainPage() {
   return (
     <div className="min-h-screen lg:bg-[url('/images/main-bg-large.png')] xl:bg-[url('/images/main-bg.png')] bg-[url('/images/main-bg-small.png')] bg-cover bg-center bg-no-repeat bg-opacity-85">
+      <BackgroundAudio />
       <div className="max-w-7xl mx-auto px-4 py-12">
-        <h1 className="text-6xl font-bold mb-8 text-center text-white font-pixelify">
-          RA-meow-N
+        <h1 className="xl:text-7xl text-6xl font-bold mb-8 text-center text-white font-pixelify tracking-wider">
+          rameown
         </h1>
       </div>
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",],
-    backgroundImage: {
-      'main-background': "url('/images/main-bg.png')",
-    },
-    fontFamily: {
-      'pixelify': ['"Pixelify Sans"', 'sans-serif'], // Use the font family name as specified
-    },
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'main-background': "url('/images/main-bg.png')",
+      },
+      fontFamily: {
+        pixelify: ['Pixelify Sans', 'sans-serif'],
+      }
+    },
   },
   plugins: [],
 }
-
