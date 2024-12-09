20c67955436c78610df47d642f4a3dca433710b1
khaulanauman <khaulanauman@gmail.com>
2024-12-09 22:21:19 +0500

    added gaming page background image

--- src/pages/gamePage.tsx
+++ src/pages/gamePage.tsx
@@ -1,10 +1,11 @@
 export default function gamePage() {
   return (
-    <div className="min-h-screen bg-gray-900 text-white">
-      <div className="max-w-7xl mx-auto px-4 py-12">
-        <h1 className="text-4xl font-bold mb-8">Gaming Hub</h1>
-        {/* Add your gaming page content here */}
-      </div>
+    <div
+      className="flex flex-col items-center min-h-screen lg:bg-[url('/images/main-bg-large.png')] 
+    xl:bg-[url('/images/game-bg.jpg')] bg-[url('/images/main-bg-small.png')] 
+    bg-cover bg-center bg-no-repeat"
+    >
+      {/* Add your gaming page content here */}
     </div>
   );
 }
