43d2027130215d7b177dd01478f061ccf7423e47
khaulanauman <khaulanauman@gmail.com>
2024-12-12 22:21:20 +0500

    added images for utensiles and ingredients

--- src/pages/gamePage.tsx
+++ src/pages/gamePage.tsx
@@ -6,6 +6,29 @@ export default function gamePage() {
     xl:bg-[url('/images/game-bg.jpg')] bg-[url('/images/main-bg-small.png')] 
     bg-cover bg-center bg-no-repeat"
     >
+      <div className="kitchen-board border-2 border-white flex justify-center items-center w-full gap-4">
+        <div className="utensils bg-red-100 p-4 rounded-md">
+          <div className="pan w-56 h-56">
+            <img src="/images/pot.png" alt="pot" />
+          </div>
+          <div className="spoon w-56 h-56">
+            <img src="/images/spoon.png" alt="spoon" />
+          </div>
+          <div className="bowl w-56 h-56">bowl</div>
+        </div>
+        <div className="board flex flex-col h-96 w-96 justify-center items-center lg:bg-[url('/images/board.png')] bg-cover bg-center bg-no-repeat"></div>
+        <div className="ingredients min-h-full bg-white p-4 rounded-md">
+          <div className="ramen w-56 h-56">
+            <img src="/images/ramen.png" alt="ramen" />
+          </div>
+          <div className="spices w-36 h-36">
+            <img src="/images/spices.png" alt="spices" />
+          </div>
+          <div className="egg w-44 h-44">
+            <img src="/images/egg.png" alt="egg" />
+          </div>
+        </div>
+      </div>
       <div className="audio-opt mb-5 bg-white rounded-md flex flex-col items-center">
         <BackgroundAudio />
         <p
@@ -14,19 +37,6 @@ export default function gamePage() {
         >
           Press to enjoy some cozy ambience
         </p>
-      </div>
-      <div className="kitchen-board border-2 border-white flex justify-center items-center w-full gap-4">
-        <div className="utensils bg-white p-4 rounded-md">
-          <div className="pan">pan</div>
-          <div className="spoon">spoon</div>
-          <div className="bowl">bowl</div>
-        </div>
-        <div className="board flex flex-col h-96 w-96 justify-center items-center lg:bg-[url('/images/board.png')] bg-cover bg-center bg-no-repeat"></div>
-        <div className="ingredients bg-white p-4 rounded-md">
-          <div className="ramen">ramen</div>
-          <div className="carrot">carrot</div>
-          <div className="carrot">carrot</div>
-        </div>
       </div>
     </div>
   );
